ecba972b6ef1685892740d9454b35a3e9cb97c98
Elena Pronichkina <prolena39@mail.ru>
2023-06-17 22:23:01 +0300

    инициализирован экземпляр класса

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,15 @@
-
+import json
+import os
+from googleapiclient.discovery import build
 
 class Channel:
     """Класс для ютуб-канала"""
-
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
+    api_key = os.getenv('API_KEY')
+    #youtube = build('youtube', 'v3', developerKey=api_key)
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         pass
