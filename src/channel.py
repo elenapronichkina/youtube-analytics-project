66b887df72c6a2cfdb8a02cced902e1c477f773c
Elena Pronichkina <prolena39@mail.ru>
2023-06-24 19:20:39 +0300

    add __init__, get_service(),  to_json()

--- src/channel.py
+++ src/channel.py
@@ -10,9 +10,35 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
         self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = self.channel["items"][0]["snippet"]["title"] #название канала
+        self.description = self.channel['items'][0]['snippet']['description'] #описание канала
+        self.url = f"https://www.youtube.com/channel/{channel_id}" #ссылка на  канал
+        self.subscriber_count = int(self.channel["items"][0]["statistics"]["subscriberCount"]) #количество подписчиков
+        self.video_count = int(self.channel["items"][0]["statistics"]["videoCount"]) #количество видео
+        self.view_count = int(self.channel["items"][0]["statistics"]["viewCount"])   #общее количество просмотров
 
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         info = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         print(info)
+
+    @classmethod
+    def get_service(cls):
+        """возвращает объект для работы с YouTube API"""
+        return cls.youtube
+    #    print(cls.youtube)
+
+    def to_json(self, filename):
+        """сохраняет в файл значения атрибутов экземпляра Channel"""
+        data = {
+            "channel_id": self.channel_id,
+            "title": self.title,
+            "description": self.description,
+            "url": self.url,
+            "subscribers_count": self.subscribers_count,
+            "video_count": self.video_count,
+            "view_count": self.view_count
+        }
+        with open(filename, "w", encoding="windows-1251") as file:
+            json.dumps(data, file, indent=2, ensure_ascii=False)
